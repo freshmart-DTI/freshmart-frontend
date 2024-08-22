259308e34263da730ea89fd5694643e16c065cfa
Jacob Irwanto <jacobirwanto@gmail.com>
2024-08-23 01:09:26 +0700

    feat: add navbar

---
+++ app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar";
+
+function Page() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default Page;
---
+++ types/navItems.d.ts
@@ -0,0 +1,5 @@
+export interface navCategories {
+  id: number;
+  name: string;
+  href: string;
+}
