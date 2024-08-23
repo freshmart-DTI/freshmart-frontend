8d93359ed3a377a5085fca768ca8cd9c67487987
Jacob Irwanto <jacobirwanto@gmail.com>
2024-08-23 16:16:10 +0700

    feat: add hero

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import Hero from "./_components/Hero";
 
 function Page() {
   return (
     <div>
       <Navbar />
+      <Hero />
       <Footer />
     </div>
   );
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,21 +3,27 @@ import { AiOutlineWhatsApp } from "react-icons/ai";
 
 function Footer() {
   return (
-    <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-      <div className="flex justify-between bg-fm-1 p-6">
-        <div className="text-fm-t1 text-sm">
-          © 2024 All rights reserved. Freshmart Pte. Ltd.
-        </div>
-        <div className="flex flex-center space-x-4">
-          <AiOutlineWhatsApp size={20} />
-          <div className="flex flex-col text-xs text-fm-t1">
-            <span>Whatsapp</span>
-            <span>+1 234-567-890</span>
+    <div className="bg-fm-1">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+        <div className="flex flex-col sm:flex-row justify-between p-6 space-y-4 sm:space-y-0">
+          <div className="text-fm-t1 text-sm text-center sm:text-left">
+            © 2024 All rights reserved. Freshmart Pte. Ltd.
           </div>
-          <AiOutlineMail size={20} />
-          <div className="flex flex-col text-xs text-fm-t1">
-            <span>Email us</span>
-            <span>admin@freshmart.com</span>
+          <div className="flex flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-4">
+            <div className="flex items-center space-x-2">
+              <AiOutlineWhatsApp size={20} />
+              <div className="flex flex-col text-xs text-fm-t1">
+                <span>Whatsapp</span>
+                <span>+1 234-567-890</span>
+              </div>
+            </div>
+            <div className="flex items-center space-x-2">
+              <AiOutlineMail size={20} />
+              <div className="flex flex-col text-xs text-fm-t1">
+                <span>Email us</span>
+                <span>admin@freshmart.com</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
