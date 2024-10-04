96ab9e5978329eed60ffa5bff7f54a632cd7f613
Rizky Ramadhan <38162104+rizkyr99@users.noreply.github.com>
2024-10-04 18:26:11 +0700

    feat(products): create product details page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,16 +1,12 @@
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
-import Hero from "./_components/Hero";
-import Product from "./_components/Product";
+import Hero from './_components/Hero';
+import Product from './_components/Product';
 
 function Page() {
   return (
-    <div>
-      <Navbar />
+    <>
       <Hero />
       <Product />
-      <Footer />
-    </div>
+    </>
   );
 }
 
